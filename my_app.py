6aacb846498c98bb7caba4ee134c32f7f1154f24
Zeraphim <diamantejc87@gmail.com>
2024-09-24 20:49:50 +0800

    added markdowns and images

--- my_app.py
+++ my_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
 
 # Title
 st.title("My Streamlit App")
@@ -15,10 +17,20 @@ st.write("This is some text")
 # Markdown
 st.markdown("This is some **markdown** text")
 
+# Markdown Stylings
+st.markdown("I'm a **bold** text")
+st.markdown("I'm an *italic* text")
+st.markdown("I'm a [link](https://www.google.com)")
 
-import matplotlib.pyplot as plt
+# Add an image from local file
+image = Image.open('assets/zuck.png')
+st.image(image, caption='This is an image')
 
+# Add an image from the internet
+image_url = 'https://www.thefamouspeople.com/profiles/images/elon-musk-1.jpg'
+st.image(image_url, caption='This is an image from the internet')
 # Simple graph
 fig, ax = plt.subplots()
 ax.plot([1, 2, 3, 4, 5])
 st.pyplot(fig)
+st.write("This is a line graph")
